61d89c2f478172ed0788e078a4c13c5be583acc9
Sergey Krivtsov <flashgangsta@gmail.com>
2023-07-13 09:17:27 +0200

    Added page title and minor content on main page

--- src/pages/cv/PageCV.tsx
+++ src/pages/cv/PageCV.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import {pageTitle} from "../../shared/consts";
 
 const PageCV = () => {
+
+    document.title = "CV" + pageTitle;
+
     return (
         <h1>CV page</h1>
     );
--- src/pages/main/PageMain.tsx
+++ src/pages/main/PageMain.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from "./styles.module.css";
 
 const PageMain = () => {
+
+    document.title = "Sergey Krivtsov | Front End Developer";
+
     return (
-        <h1>Main page</h1>
+        <div id="pageMain" className={styles.pageMain}>
+            <article>
+                <h1 className={styles.mainHeading} aria-label="Front end developer">Front End<br/>Developer</h1>
+                <p className={styles.mainHeadingSub}>Hello, I'm Siarhei. I enjoy creating interactive applications, focusing on quality, creativity, and user interaction.</p>
+            </article>
+        </div>
     );
 };
 
