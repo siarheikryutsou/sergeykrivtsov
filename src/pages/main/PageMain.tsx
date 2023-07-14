6595fc72b88c9d56c016727c92b925e1b676f782
Sergey Krivtsov <flashgangsta@gmail.com>
2023-07-14 18:21:57 +0200

    Added translations to main page; Added photo

--- src/pages/main/PageMain.tsx
+++ src/pages/main/PageMain.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from "./styles.module.css";
+import {ContextLng} from "../../app/ContextLng";
 
 const PageMain = () => {
+    const {translations} = useContext(ContextLng);
 
-    document.title = "Sergey Krivtsov | Front End Developer";
+    document.title = `${translations?.myName} | Front End Developer`;
+
+    function createMarkup(htmlText:string | undefined) {
+        return {__html: htmlText || ""};
+    }
 
     return (
         <div id="pageMain" className={styles.pageMain}>
+            <article className={styles.headingArticle}>
+                <h1 className={styles.mainHeading} aria-label={translations?.specialization} dangerouslySetInnerHTML={createMarkup(translations?.mainPageHeading)} />
+                <p className={styles.mainHeadingSub}>{translations?.mainDescription}</p>
+            </article>
             <article>
-                <h1 className={styles.mainHeading} aria-label="Front end developer">Front End<br/>Developer</h1>
-                <p className={styles.mainHeadingSub}>Hello, I'm Siarhei. I enjoy creating interactive applications, focusing on quality, creativity, and user interaction.</p>
+                <img className={styles.photo} src="/imgs/me.jpg" alt="Siarhei Kryutsou" />
             </article>
         </div>
     );
